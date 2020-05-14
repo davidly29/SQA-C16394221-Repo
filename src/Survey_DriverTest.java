7dbb4729e641c01c9fbe3e469d287c9a81d226c8
davidly29 <44612379+davidly29@users.noreply.github.com>
2020-05-14 15:51:39 +0100

    Commit 2.1 Branch: survey-creation-feature
    Details: Creation of Survey Creation Test Case with correct assertions (Failing Test)

--- src/Survey_DriverTest.java
+++ src/Survey_DriverTest.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Survey_DriverTest {
     @Test
-    public Survey shouldCreateSurvey() {
+    public void shouldCreateSurvey() {
         // TODO This test will test the successful creation of a survey object
-
-        return new Survey();
+        Survey create = Survey_Driver.surveyCreation("dog survey", "i like dogs", "you like dogs", "you dont like dogs", "they might like dogs");
+        assertEquals(create.getSurveyName(), "dog survey");
+        assertEquals(create.getQuestions().get(1), "i like dogs");
+        assertEquals(create.getQuestions().get(2), "you like dogs");
+        assertEquals(create.getQuestions().get(3), "you dont like dogs");
+        assertEquals(create.getQuestions().get(4), "they might like dogs");
     }
 
 
