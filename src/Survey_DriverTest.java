6a8bc9f638dc3ec11e8979af322e4f6669b125e3
davidly29 <44612379+davidly29@users.noreply.github.com>
2020-05-14 15:47:54 +0100

    Commit 2.0 Branch: survey-creation-feature
    Details: Creation of Survey Creation Test Case (TDD)

--- src/Survey_DriverTest.java
+++ src/Survey_DriverTest.java
@@ -1,6 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class Survey_DriverTest {
+public class Survey_DriverTest {
+    @Test
+    public Survey shouldCreateSurvey() {
+        // TODO This test will test the successful creation of a survey object
+
+        return new Survey();
+    }
 
 
 }
